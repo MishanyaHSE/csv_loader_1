5b866d3b913f382acb6048b2313e5ef11b01dbc1
mishanya <mmlobanovich_1@edu.hse.ru>
2022-06-14 18:53:15 +0300

    print fixed

--- src/main/java/loader/CSVLoader.java
+++ src/main/java/loader/CSVLoader.java
@@ -1,17 +1,18 @@
 package loader;
 
 import java.io.*;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.List;
 
 public class CSVLoader {
     private File file = null;
-    private List<List<String>> fileContent = new ArrayList<>();
-    private int maxLen;
+    private final List<List<String>> fileContent = new ArrayList<>();
+    private int maxColumns;
     private String fileName;
     private int maxCellLen = 15;
+
+    private boolean fileLoaded = false;
 
     private boolean openFile(String path) {
         try{
@@ -19,6 +20,8 @@ public class CSVLoader {
             if (file.exists()) {
                 if (path.split("\\.")[path.split("\\.").length - 1].equals("csv")) {
                     this.file = new File(path);
+                    fileLoaded = true;
+                    System.out.println("Вы успешно открыли файл!");
                 } else {
                     System.out.println("Это не csv файл! Я умею работать только с файлами расширения csv.");
                     return false;
@@ -40,10 +43,12 @@ public class CSVLoader {
             try {
                 csvReader = new BufferedReader(new FileReader(file));
                 String row;
+                fileContent.clear();
+                maxColumns = 0;
                 while ((row = csvReader.readLine()) != null) {
                     String[] data = row.split(";");
-                    if (data.length > maxLen) {
-                        maxLen = data.length;
+                    if (data.length > maxColumns) {
+                        maxColumns = data.length;
                     }
                     fileName = getFileName(path);
                     fileContent.add(List.of(data));
@@ -58,45 +63,63 @@ public class CSVLoader {
     }
 
     public void print() {
-        System.out.println(fileName + ":");
-        Formatter fmt;
-        for (var row : fileContent) {
-            fmt = new Formatter();
-            fmt.format("%1s", "|");
-            int counter = 0;
-            for (String cell : row) {
-                counter++;
-                if (cell.length() > 11) {
-                    fmt.format("%15s", cell.substring(0, 11) + "...|");
-                } else {
-                    fmt.format("%15s", cell + "|");
+        if (fileLoaded) {
+            if (file.length() == 0) {
+                System.out.println("Файл пуст.");
+            } else {
+                System.out.println(fileName + ":");
+                printLine(maxColumns);
+                Formatter fmt;
+                for (var row : fileContent) {
+                    fmt = new Formatter();
+                    fmt.format("%1s", "|");
+                    int counter = 0;
+                    for (String cell : row) {
+                        counter++;
+                        if (cell.length() > maxCellLen - 1) {
+                            fmt.format("%" + maxCellLen + "s", cell.substring(0, maxCellLen - 4) + "...|");
+                        } else {
+                            fmt.format("%" + maxCellLen + "s", cell + "|");
+                        }
+                    }
+                    while (counter < maxColumns) {
+                        fmt.format("%" + maxCellLen + "s", "|");
+                        counter++;
+                    }
+                    System.out.println(fmt);
                 }
             }
-            while(counter < maxLen) {
-                fmt.format("%15s", "|");
-                counter++;
-            }
-            System.out.println(fmt);
+            printLine(maxColumns);
+        } else {
+            System.out.println("Сначала выберите файл!");
         }
-        System.out.println("---------------------------------------------------------");
     }
 
     public void print(int numOfColumn) {
-        Formatter fmt = new Formatter();
-        for (var row : fileContent) {
-            for (int i = 0; i < row.size(); ++i) {
-                if (i == numOfColumn - 1) {
-                    fmt.format("%10s", row.get(i) + "\n");
+        if (fileLoaded) {
+            Formatter fmt = new Formatter();
+            for (int j = 0; j < fileContent.size(); ++j) {
+                for (int i = 0; i < fileContent.get(j).size(); ++i) {
+                    if (i == numOfColumn - 1) {
+                        fmt.format("%1s", "\n|");
+                        if (fileContent.get(j).get(i).length() > maxCellLen - 1) {
+                            fmt.format("%" + maxCellLen + "s", fileContent.get(j).get(i).substring(0, maxCellLen - 4) + "..|");
+                        } else {
+                            fmt.format("%" + maxCellLen + "s", fileContent.get(j).get(i) + "|");
+                        }
+                    }
                 }
             }
-        }
-        if (fmt.toString().equals("")) {
-            System.out.println("Колонка " + numOfColumn + " пуста.");
+            if (fmt.toString().equals("")) {
+                System.out.println("Столбец " + numOfColumn + " пуст.");
+            } else {
+                System.out.println("Столбец " + numOfColumn + ":");
+                System.out.println(fmt);
+            }
+            printLine(1);
         } else {
-            System.out.println("Столбец " + numOfColumn + ":");
-            System.out.println(fmt);
+            System.out.println("Сначала выберите файл!");
         }
-        System.out.println("---------------------------------------------------------");
     }
 
     private String getFileName (String path) {
@@ -106,5 +129,29 @@ public class CSVLoader {
         } else {
             return path.split("/")[path.split("/").length - 1];
         }
+    }
+
+    public void changeLen(int newLen) {
+        maxCellLen = newLen;
+    }
+
+    private void printLine(int numOfColumns) {
+        int numOfSlashes = maxCellLen * numOfColumns + 1;
+        for (int i = 0; i < numOfSlashes; ++i) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getMaxColumns() {
+        return maxColumns;
+    }
+
+    public int getMaxCellLen() {
+        return maxCellLen;
     }
 }
--- src/main/java/utils/ConsoleReader.java
+++ src/main/java/utils/ConsoleReader.java
@@ -30,6 +30,8 @@ public class ConsoleReader {
                        for (int i = 1; i < splitLine.length; ++i) {
                            loader.print(Integer.parseInt(splitLine[i]));
                        }
+                   } else {
+                       System.out.println("После команды указывайте только целые числа - номера столбцов.");
                    }
                 }
                 break;
@@ -37,6 +39,22 @@ public class ConsoleReader {
                 printAllCommands();
                 break;
             case "":
+                break;
+            case "/cell_len":
+                if (splitLine.length == 1) {
+                    System.out.println("После команды укажите длину одной клетки! (от 5 до 20)");
+                } else if (splitLine.length > 2) {
+                    System.out.println("После команды укажите один аргумент - длину одной клетки" +
+                            "(целое число от 5 до 20).");
+                } else if (isAllNumeric(splitLine)) {
+                    int newLen = Integer.parseInt(splitLine[1]);
+                    if (newLen > 4 && newLen < 21) {
+                        loader.changeLen(newLen);
+                        System.out.println("Вы успешно изменили длину клетки! Новая длина: " + newLen);
+                    } else {
+                        System.out.println("Длина клетки должна быть от 5 до 20!");
+                    }
+                }
                 break;
             default:
                 System.out.println("Не могу вас понять. Чтобы получить список комманд, введите /help.");
@@ -46,7 +64,6 @@ public class ConsoleReader {
     private boolean isAllNumeric(String[] splitLine) {
         for (int i = 1; i < splitLine.length; ++i) {
             if (!splitLine[i].matches("[0-9]+")) {
-                System.out.println("После команды /print должны вводиться только целые числа!");
                 return false;
             }
         }
@@ -54,10 +71,12 @@ public class ConsoleReader {
     }
 
     private void printAllCommands() {
-        System.out.println("Комманды: \n" +
-                "/load path - загружает csv файл. Path - полный путь к файлу.\n" +
-                "/print - печатает содержимое выбранной таблицы\n" +
-                "/print n1 n2 - печатает столбцы n1 и n2 выбранной таблицы\n" +
-                "/exit - выход из программы.");
+        System.out.println("""
+                Комманды:
+                /load path - загружает csv файл. Path - полный путь к файлу.
+                /print - печатает содержимое выбранной таблицы
+                /print n1 n2 - печатает столбцы n1 и n2 выбранной таблицы
+                /cell_len n - изменить длину одной клетки на n
+                /exit - выход из программы.""");
     }
 }
